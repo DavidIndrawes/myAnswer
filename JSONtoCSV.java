1199a32651bc19dc68b9f598140f7f00a5e2a9e2
DavidIndrawes <david.indrawes@hotmail.com>
2016-07-28 10:15:08 -0700

    Add files via upload

--- JSONtoCSV.java
+++ JSONtoCSV.java
@@ -15,13 +15,15 @@ public class JSONtoCSV {
 		
 		String url = "http://api.goeuro.com/api/v2/position/suggest/en/"+args[0];
 		
-		
+		// fetch the JSON array corresponding to each city from the server
 		JSONArray myArray = fetchJsonFromURL(url);
 		
 		try {
+			// parse the JSON data and save it to a CSV file
 			parseAndSaveJSON(myArray,args[0]);
 		}  catch (IOException e) {
-			e.printStackTrace();
+			e.printStackTrace();	// in case cannot create new file
+			System.out.println("cannot create the csv result file, please make sure that no file with the same name is exist and being used by other application");
 		} 
 		
 	}
@@ -32,9 +34,11 @@ public class JSONtoCSV {
 		try{
 			URL url = new URL(s);
 			InputStream is = url.openStream();
-			String data = IOUtils.toString(is, StandardCharsets.UTF_8);
+			String data = IOUtils.toString(is, StandardCharsets.UTF_8);	// get the JSON document in String
 			IOUtils.closeQuietly(is);
-			myArray = new JSONArray(data);
+			
+			myArray = new JSONArray(data);	// create new JSON array with the fetched data
+			
 		} catch (IOException e) {
 			// in case of malformed URL or cannot connect to the server, return empty JSON array to create empty CSV file
 			System.out.println("the server is down or malformed URL");
@@ -50,13 +54,14 @@ public class JSONtoCSV {
 		File csvFile = new File(city+".csv");
 		FileWriter writer = new FileWriter(csvFile);
 		
-		JSONObject myObject;
-		JSONObject position;
-		writer.write("Id"+","+"Name"+","+"Type"+","+"Latitude"+","+"Longitude\n");
+		writer.write("Id"+","+"Name"+","+"Type"+","+"Latitude"+","+"Longitude\n");	// table heading
+		
+		JSONObject myObject, position;
+		
 		for(int i=0;i<myArray.length();i++){
-			myObject= myArray.optJSONObject(i);
+			myObject= myArray.optJSONObject(i);		// iterate over the objects in the array
 			writer.write(myObject.optInt("_id")+","+myObject.optString("name")+","+myObject.optString("type"));
-			position = myObject.optJSONObject("geo_position");
+			position = myObject.optJSONObject("geo_position");	// for each object get the position object 
 			writer.write(","+position.optDouble("latitude")+","+position.optDouble("longitude")+"\n");
 		}
 		
